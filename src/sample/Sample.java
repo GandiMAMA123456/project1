4a8920f901319acbc53a154b43cde3a8d847ff44
Venu Gopal A <venu_a@trisysit.com>
2023-05-28 23:16:16 +0530

    message

---
+++ src/sample/Sample.java
@@ -0,0 +1,26 @@
+package sample;
+
+public class Sample {
+	public static void main(String[] args)
+}
+	int sum;
+	while=0;
+	{
+		int sum=0;
+		while(0>9);
+		{
+			int d=no%10;
+			sum=sum+d;
+			no=no/10;
+		}
+		if(num==sum)
+		{
+			if(sum==1||sum==7)
+			{
+		System.out.println(its a happy number);
+			else
+				System.out.println(it is not a happy number)
+	}
+
+}
+	
